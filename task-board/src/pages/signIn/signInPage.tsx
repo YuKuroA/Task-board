a06a64e7a64e0dd05676481aead17c27760352e7
YuKuroA <andriymailforwork@gmail.com>
2023-06-15 11:59:06 +0300

    Add some changes

---
+++ task-board/src/pages/signIn/signInPage.tsx
@@ -0,0 +1,18 @@
+import SignIn from "../../components/signIn/signIn"
+import './signInPage.css'
+
+interface SignInPageProps {
+    passLogin: (login:string) => void
+}
+
+const SignInPage: React.FC<SignInPageProps> = ({passLogin}) => {
+    const signInPage = (
+        <div className="signInPage">
+            <SignIn passLogin={passLogin} />
+        </div>
+    )
+
+    return signInPage
+}
+
+export default SignInPage
